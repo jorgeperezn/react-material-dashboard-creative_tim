28e5f316d2537da94881ab56f7e77e4e55221a37
Jorge Perez <jorge.perez@codescrum.com>
2019-12-05 11:47:33 -0500

    Updated footer wording.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -43,13 +43,13 @@ export default function Footer(props) {
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
             <a
-              href="https://www.creative-tim.com?ref=mdr-footer"
+              href="https://www.codescrum.com"
               target="_blank"
               className={classes.a}
             >
-              Creative Tim
+              Codescrum
             </a>
-            , made with love for a better web
+            , LTD 2011 - Present, All rights reserved
           </span>
         </p>
       </div>
